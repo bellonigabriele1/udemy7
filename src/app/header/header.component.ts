160be8160dc995c1515b87fc7faed112d5c2b33b
bellonigabriele1 <bellonigabriele1@gmail.com>
2020-03-26 08:44:50 -0400

    prime modifiche, funziona tutto

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+  @Output() activeSectionEvent = new EventEmitter<string>();
+  activeSection: string;
+
+  onRecipes() {
+    console.log('onRecipes()');
+    this.activeSection = 'recipes';
+    this.activeSectionEvent.emit(this.activeSection);
+  }
+
+  onShoppingList() {
+    console.log('onShoppingList()');
+  }
 
 }
